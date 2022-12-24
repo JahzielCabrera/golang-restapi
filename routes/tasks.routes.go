ff95f7ff5c8cb20f872e9b9cd1b6dd1d075afc53
Jahziel Cabrera <jahziel_oswaldo@hotmail.com>
2022-12-24 02:24:00 -0600

    Taks routes added

--- routes/tasks.routes.go
+++ routes/tasks.routes.go
@@ -1,21 +1,55 @@
 package routes
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jahzielcabrera/golang-restapi/db"
+	"github.com/jahzielcabrera/golang-restapi/models"
 )
 
 func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GET Tasks Handler"))
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(&tasks)
 }
 
 func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GET Task Handler"))
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Tarea no encontrada"))
+		return
+	}
+	json.NewEncoder(w).Encode(&task)
 }
 
 func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("POST Task Handler"))
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+	createdTask := db.DB.Create(&task)
+	err := createdTask.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	json.NewEncoder(w).Encode(&task)
 }
 
 func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("DELETE Task Handler"))
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Tarea no encontrada"))
+		return
+	}
+	db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte("Tarea Eliminada"))
 }
